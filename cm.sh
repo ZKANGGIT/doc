00c19eb9547d1bf4ee8d77a766f7d18d92103998
zuokang <974825200@qq.com>
2023-03-13 17:20:21 +0800

    asda

---
+++ cm.sh
@@ -0,0 +1,5 @@
+sudo yum install git python3-pip build-essential wget python3-dev python3-venv \
+    python3-wheel libfreetype6-dev libxml2-dev libzip-dev libldap2-dev libsasl2-dev \
+    python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev \
+    libxslt1-dev libldap2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev \
+    liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev
