cb3b187b11c48a6fb50c0febfccf87056fabc7e2
zuokang <974825200@qq.com>
2023-03-13 17:23:48 +0800

    asda

--- cm.sh
+++ cm.sh
@@ -1,5 +1,2 @@
-sudo yum install git python3-pip build-essential wget python3-dev python3-venv \
-    python3-wheel libfreetype6-dev libxml2-dev libzip-dev libldap2-dev libsasl2-dev \
-    python3-setuptools node-less libjpeg-dev zlib1g-dev libpq-dev \
-    libxslt1-dev libldap2-dev libtiff5-dev libjpeg8-dev libopenjp2-7-dev \
-    liblcms2-dev libwebp-dev libharfbuzz-dev libfribidi-dev libxcb1-dev
+sudo yum install gcc gcc-c++ openssl-devel bzip2-devel libffi-devel libxml2-devel libxslt-devel libjpeg-turbo-devel openldap-devel  postgresql postgresql-devel python-devel -y
+
